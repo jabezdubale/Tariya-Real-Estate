bc262beb4549af0851be2a76f1b56d76b5292ca4
jabezdubale <jabezdubale6@gmail.com>
2022-09-13 10:38:58 +0300

    first commit of 9/13/2022

--- src/main/java/com/tariyarealestate/tariyarealestate/services/AccountService.java
+++ src/main/java/com/tariyarealestate/tariyarealestate/services/AccountService.java
@@ -10,6 +10,7 @@ public class AccountService {
 
     @Autowired
     private AccountRepository accountRepository;
+
     public Account createAccount (Account account){
         return accountRepository.save(account);
     }
