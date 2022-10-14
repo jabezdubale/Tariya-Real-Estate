bc9bf39ba0b97c06879dda447194eed60c0d6e6f
jabezdubale <jabezdubale6@gmail.com>
2022-10-14 10:13:09 +0300

    Added delete account feature

--- src/main/java/com/tariyarealestate/tariyarealestate/controllers/AccountController.java
+++ src/main/java/com/tariyarealestate/tariyarealestate/controllers/AccountController.java
@@ -1,6 +1,7 @@
 package com.tariyarealestate.tariyarealestate.controllers;
 
 import com.tariyarealestate.tariyarealestate.domains.Account;
+import com.tariyarealestate.tariyarealestate.dto.AccountDelete;
 import com.tariyarealestate.tariyarealestate.dto.AccountEditor;
 import com.tariyarealestate.tariyarealestate.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,11 @@ public class AccountController {
     @PostMapping("/api/account/edit")
     public Account editAccount(@RequestBody AccountEditor accountEditor) throws Exception {
         return accountService.editAccount(accountEditor);
+    }
+
+    @PostMapping("/api/account/delete")
+    public void deleteAccount(@RequestBody AccountDelete acDelete) throws Exception {
+        accountService.deleteAccount(acDelete);
     }
 
 }
--- src/main/java/com/tariyarealestate/tariyarealestate/services/AccountService.java
+++ src/main/java/com/tariyarealestate/tariyarealestate/services/AccountService.java
@@ -1,7 +1,10 @@
 package com.tariyarealestate.tariyarealestate.services;
 
 import com.tariyarealestate.tariyarealestate.domains.Account;
+import com.tariyarealestate.tariyarealestate.domains.RealEstate;
+import com.tariyarealestate.tariyarealestate.dto.AccountDelete;
 import com.tariyarealestate.tariyarealestate.dto.AccountEditor;
+import com.tariyarealestate.tariyarealestate.dto.RealEstateDelete;
 import com.tariyarealestate.tariyarealestate.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -47,6 +50,16 @@ public class  AccountService {
         }
 
         return accountRepository.save(edittedAccount);
+    }
+
+    public void deleteAccount(AccountDelete acDelete) throws Exception {
+        Account deleteAcct = accountRepository.findById(acDelete.getDeleteId()).get();
+        if (deleteAcct == null){
+            throw new Exception();
+        }
+        else {
+            accountRepository.deleteById(acDelete.getDeleteId());
+        }
     }
 
 }
